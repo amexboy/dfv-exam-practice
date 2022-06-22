eb8188aaa6ab09aedf24f21c8c75c6b2614cd266
Amanuel Nega <negaamanuel@gmail.com>
2022-06-22 21:50:22 +0200

    Added text to json converter

---
+++ pages/generator.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row>
+    <!-- <v-col>{{ source }}</v-col> -->
+    <!-- <v-col>{{ result }}</v-col> -->
+    <v-col>{{ questions }}</v-col>
+    <!-- <v-col>
+      <question
+        v-for="question in result"
+        :key="question.id"
+        :question="question"
+      />
+    </v-col> -->
+  </v-row>
+</template>
+
+<script>
+// import Question from '~/components/Question.vue'
+import source from '~/static/test.text'
+export default {
+//   components: { Question },
+  data () {
+    return {
+      source
+    }
+  },
+  computed: {
+    matches () {
+      const pattern = /((?:Aerodynamics)|(?:Free ?fall)|(?:Air traffic law)|(?:Meteorology)|(?:Equipment)|(?:Human Performance)|(?:Behaviour in special circumstances)).*?Question No\.? ([0-9]+)(.*?)(\(.*?\)).*?a\)(.*?)b\)(.*?)c\)(.*?)d\)(.*?)Correct answer: ([a-e])/sigm
+      return [...source.matchAll(pattern)]
+    },
+    result () {
+      console.log(this.matches)
+      return this.matches.map(m => ({
+        category: m[1].trim(),
+        q_id: m[2].trim(),
+        question: m[3].trim(),
+        choices: [
+          { choice: 'A', value: m[5].trim() },
+          { choice: 'B', value: m[6].trim() },
+          { choice: 'C', value: m[7].trim() },
+          { choice: 'D', value: m[8].trim() }],
+        answer: m[9].trim().toUpperCase(),
+        original: m[0]
+      }))
+    },
+    questions () {
+      return this.result.map(r => ({ q: r.q_id, c: r.category }))
+    }
+  }
+}
+</script>
